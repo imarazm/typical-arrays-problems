446b568f5d0d9018f345b90c410735e923eefe8f
Artem Razmyslovich <marazm69@gmail.com>
2020-09-29 15:44:12 +0300

    feat: add functions for min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let arrMin = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < arrMin) {
+        arrMin = array[i];
+      }
+    }
+    return min = arrMin;
+    // return Math.min.apply(null, array);
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let arrMax = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > arrMax) {
+        arrMax = array[i];
+      }
+    }
+    return min = arrMax;
+    // return Math.max.apply(null, array);
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let arrSum = 0;
+    for (let i = 0; i < array.length; i++) {
+      arrSum += array[i];
+    }
+    return avg = arrSum / array.length;
+    // return avg = array.reduce((previous, current) => current += previous) / array.length;
+  }
 }
